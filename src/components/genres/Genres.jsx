785f253edac4fdd15c439f396a1126ecc8ae2bd8
shahdabmalik <shahdab888@gmail.com>
2023-05-01 18:41:14 +0530

    Added flex-wrap in genres

--- src/components/genres/Genres.jsx
+++ src/components/genres/Genres.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux'
 const Genres = ({ data }) => {
     const { genres } = useSelector((state) => state.home)
     return (
-        <div className='text-xs flex gap-2 mt-3' >
+        <div className='text-xs flex gap-2 mt-3 flex-wrap' >
             {data?.map((genre, index) => {
                 return (
                     <div className='bg-red-700 py-0.5 px-1 rounded-sm' key={index}>{genres[genre]?.name}</div>
